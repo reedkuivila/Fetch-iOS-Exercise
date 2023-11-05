310a39e804846b561825e37050f5a66e02306b30
reedkuivila <reedkuivila@uchicago.edu>
2023-11-05 14:46:14 -0600

    initial commit

---
+++ Fetch iOS Assessment/Fetch iOS Assessment/Fetch_iOS_AssessmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fetch_iOS_AssessmentApp.swift
+//  Fetch iOS Assessment
+//
+//  Created by reed kuivila on 11/5/23.
+//
+
+import SwiftUI
+
+@main
+struct Fetch_iOS_AssessmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
